a649d9e0b150f9e440755faf50229ee28488890e
tanisha2430 <tanishasrms24@gmail.com>
2025-12-30 08:18:31 +0000

    Skills section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -41,30 +41,92 @@ actively to team success.
                 <motion.button>WHAT AM I GOOD AT?</motion.button>
             </motion.div>
         </motion.div>
-        <motion.div className="listContainer" variants={variants} >
-            <motion.div className="box" whileHover={{backgroundColor:"rgb(15, 49, 79)" }}>
-                <motion.h2>Programming Languages</motion.h2>
-                <motion.p>Java , Python</motion.p>
-                
-            </motion.div>
-            <motion.div className="box" whileHover={{backgroundColor:"rgb(15, 49, 79)" }}>
-                <motion.h2>Frontend Development</motion.h2>
-                <motion.p>HTML,CSS,<br />JavaScript,React</motion.p>
-               
-            </motion.div>
-            <motion.div className="box" whileHover={{backgroundColor:"rgb(15, 49, 79)" }}>
-                <motion.h2>Styling & Animations</motion.h2>
-                <motion.p>Bootstrap,Tailwind CSS,Framer motion</motion.p>
-               
-            </motion.div>
-            <motion.div className="box" whileHover={{backgroundColor:"rgb(15, 49, 79)" }}>
-                <motion.h2>Non-Technical Skills</motion.h2>
-                <motion.p>Presentation,Communication, 
-                     <br />
-                   Adaptability, <br /> Effective scheduling,Prioritization</motion.p>
-                
-            </motion.div>
-        </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+
+  <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>Frontend Development</motion.h2>
+    <motion.p>
+      HTML, CSS, JavaScript, TypeScript<br />
+      React.js, Redux Toolkit, Context API<br />
+      REST API Integration
+    </motion.p>
+    
+  </motion.div>
+
+  <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>Styling</motion.h2>
+    <motion.p>
+      Tailwind CSS, SCSS, NativeWind<br />
+      Framer Motion
+    </motion.p>
+    
+  </motion.div>
+
+  <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>Mobile App Development</motion.h2>
+    <motion.p>
+      React Native, Expo<br />
+      Android Studio, Xcode<br />
+      Deep Linking, React Navigation
+    </motion.p>
+    
+  </motion.div>
+
+  <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>Backend</motion.h2>
+    <motion.p>
+      Node.js, Express.js<br />
+      AWS Serverless Microservices Architecture<br />
+      MySQL, MongoDB, Firebase<br />
+      Sequelize
+    </motion.p>
+    
+  </motion.div>
+
+  <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>Cloud & DevOps</motion.h2>
+    <motion.p>
+      AWS services: EC2, S3, IAM, SSM, CloudWatch, Lambda, RDS, API Gateway, CodePipeline, Amplify,
+      <br />
+      Docker (Basic)
+    </motion.p>
+    
+  </motion.div>
+
+  <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>FinTech</motion.h2>
+    <motion.p>
+      Payment Gateways (Adyen)<br />
+      PCI Compliance Basics
+    </motion.p>
+    
+  </motion.div>
+
+   <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>Tools</motion.h2>
+    <motion.p>
+      Git, GitHub, Postman, JIRA, Confluence
+    </motion.p>
+    
+  </motion.div>
+  <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>Monitoring and Analytics</motion.h2>
+    <motion.p>
+      Grafana, InfluxDB, APIs
+    </motion.p>
+    
+  </motion.div>
+  <motion.div className="box" whileHover={{ backgroundColor: "rgb(15, 49, 79)" }}>
+    <motion.h2>Soft Skills</motion.h2>
+    <motion.p>
+     Communication, Collaboration, Prioritization
+    </motion.p>
+    
+  </motion.div>
+
+</motion.div>
+
+
       
     </motion.div>
   )
